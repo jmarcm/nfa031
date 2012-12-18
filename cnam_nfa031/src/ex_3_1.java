92e9e08e7407235716df09da5ff63e61fdc3296b
jmarcm <mjean_marc@hotmail.com>
2012-12-18 08:24:55 +0100

    Première version

---
+++ cnam_nfa031/src/ex_3_1.java
@@ -0,0 +1,14 @@
+
+public class ex_3_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print ("Donner le nombre de séances : ");
+		int n = Lire.i();
+		System.out.print ("Le nombre est " +n);
+	}
+
+}
